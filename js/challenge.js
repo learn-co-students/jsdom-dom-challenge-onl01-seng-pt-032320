26696333c8cd7e8be73dc267d69734554e5e777b
arbrown110 <arbrown110@gmail.com>
2020-09-25 17:27:58 -0400

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,6 @@
+let domChallenge = setInterval() => {
+    docmuent.addEventListener('DOMContentLoaded', (event) => {
+        let counter = 0; 
+        while (counter <100){ counter++ }
+    })
+     }, 1000);
